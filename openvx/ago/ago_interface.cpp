3f407c7dddab9da0275b509d9213eedfe8a4f993
lcskrishna <clolla@amd.com>
2017-08-10 10:00:12 -0700

    fixed conflicts during sync

--- openvx/ago/ago_interface.cpp
+++ openvx/ago/ago_interface.cpp
@@ -2065,19 +2065,6 @@ int agoExecuteGraph(AgoGraph * graph)
 					}
 				}
 				else {
-				for (vx_uint32 i = 0; i < node->paramCount; i++) {
-					AgoData * data = node->paramList[i];
-					if (data && (node->parameters[i].direction == VX_INPUT || node->parameters[i].direction == VX_BIDIRECTIONAL)) {
-						auto dataToSync = (data->ref.type == VX_TYPE_IMAGE && data->u.img.isROI) ? data->u.img.roiMasterImage : data;
-						status = agoDataSyncFromGpuToCpu(graph, node, dataToSync);
-						for (vx_uint32 j = 0; !status && j < dataToSync->numChildren; j++) {
-							AgoData * jdata = dataToSync->children[j];
-							if (jdata)
-								status = agoDataSyncFromGpuToCpu(graph, node, jdata);
-						}
-					}
-				}
-				else {
 					for (vx_uint32 i = 0; i < node->paramCount; i++) {
 						AgoData * data = node->paramList[i];
 						if (data && (node->parameters[i].direction == VX_INPUT || node->parameters[i].direction == VX_BIDIRECTIONAL)) {
